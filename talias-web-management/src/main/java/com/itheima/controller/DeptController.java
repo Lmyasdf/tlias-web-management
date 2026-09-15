32c564a32e3ddb87f2e0bec606fc75c5e2f5b8a5
Lmyasdf <1901187412@qq.com>
2026-09-15 10:23:53 +0800

    feat: 配置日志文件，学习操作日志

--- talias-web-management/src/main/java/com/itheima/controller/DeptController.java
+++ talias-web-management/src/main/java/com/itheima/controller/DeptController.java
@@ -3,31 +3,38 @@ package com.itheima.controller;
 import com.itheima.pojo.Dept;
 import com.itheima.pojo.Result;
 import com.itheima.service.DeptService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j//日志
 @RestController
 @RequestMapping("/depts")
 public class DeptController {
+    //private static final Logger log = LoggerFactory.getLogger(DeptController.class);
     @Autowired
     private DeptService deptService;
 
+    @GetMapping
     public Result findAll(){
+        log.info("查询全部部门数据");
         List<Dept> list = deptService.findAll();
         return Result.success(list);
     }
 
     //删除部门
+    @DeleteMapping
     public Result delete(Integer id){
         deptService.delete(id);
         return Result.success();
     }
 
     //添加部门
+    @PostMapping
     public Result insert(@RequestBody Dept dept){
-        System.out.println("添加部门" + dept);
+        log.info("新增部门：{}",dept);
         deptService.insert(dept);
         return Result.success();
     }
@@ -43,6 +50,7 @@ public class DeptController {
     //修改部门
     @PutMapping("/setData")
     public Result setData(@RequestBody Dept dept){
+        log.info("修改部门：{}",dept);
         deptService.set(dept);
         return Result.success();
     }
