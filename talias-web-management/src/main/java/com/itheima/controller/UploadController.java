15711ed5de988f1d0d6b3938f2b2a5d82c6a2939
Lmyasdf <1901187412@qq.com>
2026-09-18 18:27:56 +0800

    feat：文件上传，并存储到本地磁盘

---
+++ talias-web-management/src/main/java/com/itheima/controller/UploadController.java
@@ -0,0 +1,28 @@
+package com.itheima.controller;
+
+import com.itheima.pojo.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+@RestController
+public class UploadController{
+    @PostMapping("/upload")
+    public Result upload(String name, Integer age, MultipartFile file) throws Exception {
+        log.info("name: {}, age: {}, file: {}", name, age, file);
+        String originalFilename = file.getOriginalFilename();
+        //创建随机文件名
+        String extension = originalFilename.substring(originalFilename.lastIndexOf('.'));
+        String newFileName = UUID.randomUUID().toString() + extension;
+
+        //保存文件
+        file.transferTo(new File("D:/images/" + newFileName));
+        return Result.success();
+    }
+}
